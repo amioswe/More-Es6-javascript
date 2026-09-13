73b9a7b50e9a70619bdee83600ff35e9cb0c9eba
amioswe <amioswe@gmail.com>
2026-09-13 15:17:30 +0600

    pass by ref

---
+++ pass_by-ref.js
@@ -0,0 +1,13 @@
+let student={
+    name:'AMIODAS',
+    age:20
+}
+
+ function myfunc(data){
+
+    data.name='Mikhaile';
+console.log(data.name);
+
+}
+myfunc(student);
+console.log(student);
---
+++ passby_value.js
@@ -0,0 +1,9 @@
+let name='AMIODAS';
+let age=20;
+function myfunc(myname,myage){
+  myname='Mikhaile';
+  myage=40;
+  console.log(myname,myage);
+}
+myfunc(name,age);
+console.log(name,age);
